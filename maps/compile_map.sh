a46860c3ff5439f26713adbe87daf254472e38ba
tohtoris <tohtoris@gmail.com>
2012-03-25 15:45:29 +0300

    new buildings and objects, new lights, add textures, compile script

---
+++ maps/compile_map.sh
@@ -0,0 +1,3 @@
+./q3map2.x86 -fs_game truecombat -fs_basepath "/home/antti/pelit/openarena-0.8.1" -meta -v "cetape.map"
+./q3map2.x86 -fs_game truecombat -fs_basepath "/home/antti/pelit/openarena-0.8.1" -vis -v "cetape.map"
+./q3map2.x86 -fs_game truecombat -fs_basepath "/home/antti/pelit/openarena-0.8.1" -light -fast -patchshadows -samples 3 -bounce 8 -gamma 2 -compensate 4 -dirty -v "cetape.map"
